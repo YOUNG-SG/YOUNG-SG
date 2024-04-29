50562252c091b04a1725ab86b03db9e5ffefdd4d
박규리 <dw05131@naver.com>
2024-04-29 15:20:31 +0900

    feat: 로그인 API (2) - 토큰 저장/전역상태/로그인 성공

--- frontend/src/pages/Login/KakaoLoginPage.tsx
+++ frontend/src/pages/Login/KakaoLoginPage.tsx
@@ -1,19 +1,20 @@
 import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import { kakaoLoginFetch } from "@/services/Login";
 import { tokenStore } from "@/store/tokenStore";
-// import { useNavigate } from "react-router-dom";
 
 const KakaoLoginPage = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const queryParams = new URLSearchParams(location.search);
   const code = queryParams.get("code");
 
   async function getToken(code: string) {
     try {
-      const token = await kakaoLoginFetch(code);
-      tokenStore.setState({ token: token });
-      // navigate("/");
+      const token = await kakaoLoginFetch(code); // { accessToken: "", refreshToken: "" }
+      tokenStore.setState({ token: token.accessToken }); // accessToken
+      alert("로그인 되었습니다.");
+      navigate("/");
     } catch (err) {
       console.log(err);
     }
--- frontend/src/services/Login.ts
+++ frontend/src/services/Login.ts
@@ -2,6 +2,5 @@ import { axiosRequest } from "./axios";
 
 export const kakaoLoginFetch = async (code: string) => {
   const res = await axiosRequest.get(`/api/oauth/token?code=${code}`);
-  console.log(res.headers.authorization);
-  return res.headers.authorization;
+  return res.data;
 };
