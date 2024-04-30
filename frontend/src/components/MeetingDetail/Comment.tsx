71e6c213e6ec0361b7c16964ebbf707efdb30ad3
박규리 <dw05131@naver.com>
2024-04-30 17:50:56 +0900

    feat: 회의상세 댓글 조회, 작성, 삭제 API 연결

--- frontend/src/components/MeetingDetail/Comment.tsx
+++ frontend/src/components/MeetingDetail/Comment.tsx
@@ -1,29 +1,46 @@
 import UserInfo from "./UserInfo";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { deleteComment } from "@/services/MeetingDetail";
 
-type CommentType = {
-  profile: string;
-  nickname: string;
+export type CommentType = {
+  commentId: number;
   content: string;
-  isWriter: boolean;
+  createAt: string;
+  memberId: number;
+  nickname: string;
+  profileUrl: string;
 };
 
-const Comment: React.FC<{ comment: CommentType }> = (props) => {
-  const comment = props.comment;
+const Comment: React.FC<{
+  comment: CommentType;
+  meetingDetailId: string | undefined;
+  myMemberId: number;
+}> = ({ comment, meetingDetailId, myMemberId }) => {
+  const queryClient = useQueryClient();
+  // const comment = props.comment;
+  const isWriter = myMemberId === comment.memberId;
+
+  const { mutate: delComment } = useMutation({
+    mutationFn: deleteComment,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["comments", meetingDetailId] });
+    },
+  });
 
   return (
     <div className="flex w-full justify-between">
       <div className="w-[160px]">
-        <UserInfo img={comment.profile} nickname={comment.nickname} />
+        <UserInfo img={comment.profileUrl} nickname={comment.nickname} />
       </div>
       <div className="text-[20px] mt-[1px] font-l" style={{ width: "calc(100% - 190px)" }}>
         {comment.content}
       </div>
-      {comment.isWriter && (
+      {isWriter && (
         <div
           className="w-[30px] min-w-[30px] mt-[4px] text-[12px] font-l cursor-pointer"
           onClick={() => {
             if (confirm("댓글을 삭제하시겠어요?")) {
-              // [API] 삭제
+              delComment(comment.commentId);
             }
           }}
         >
