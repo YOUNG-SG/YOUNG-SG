e919f667eff919952ba9c25eb7a735db8f3de6e7
정은진 <jin5718@naver.com>
2024-05-02 10:40:36 +0900

    feat : redirect URL 수정

--- backend/src/main/java/com/d208/AIclerk/member/service/MemberServiceImpl.java
+++ backend/src/main/java/com/d208/AIclerk/member/service/MemberServiceImpl.java
@@ -41,6 +41,9 @@ public class MemberServiceImpl implements MemberService{
     @Value("${KAKAO_CLIENT_SECRET}")
     String SecretKey;
 
+    @Value("${REDIRECT_URI}")
+    String RedirectUri;
+
     private int accessTokenMinute = 60;
     private int refreshTokenMinute = 3000;
 
@@ -58,7 +61,7 @@ public class MemberServiceImpl implements MemberService{
         MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
         params.add("grant_type", "authorization_code");
         params.add("client_id", ClientKey);
-        params.add("redirect_uri", "http://localhost:5173/oauth/callback/kakao");
+        params.add("redirect_uri", RedirectUri);
         params.add("code", code);
         params.add("client_secret", SecretKey);
 
