36fb16a73183f7e941c6a2659b5c2138a5ec3cb0
박규리 <dw05131@naver.com>
2024-05-03 16:58:57 +0900

    fix: 폴더 TotalTime 삭제

--- frontend/src/components/MyPage/Folder.tsx
+++ frontend/src/components/MyPage/Folder.tsx
@@ -6,9 +6,11 @@ const Folder = () => {
     >
       {/* FIXME 회의명 글자 넘치는 경우 ... */}
       <div className="text-[32px] font-extrabold">자율 PJT 개발 회의</div>
+      {/* FIXME total time 안 쓰면 아래 줄 지우기 */}
       <div className="flex justify-between text-[#CCCCCC]">
         <div>24.04.23</div>
-        <div>30분</div>
+        {/* total time */}
+        {/* <div>30분</div> */}
       </div>
     </div>
   );
