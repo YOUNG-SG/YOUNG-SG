db9ea690a6274a233f510770a3b68e097077c36f
Lim Kyuhwan <cnn482@gmail.com>
2024-05-03 13:48:08 +0900

    feat: memberMeeting 엔터티 수정 및 회의 상세 리스트

--- backend/src/main/java/com/d208/AIclerk/entity/MemberMeeting.java
+++ backend/src/main/java/com/d208/AIclerk/entity/MemberMeeting.java
@@ -16,13 +16,16 @@ public class MemberMeeting {
     @Column(name = "member_meeting_id")
     private Long id;
 
+    @Column(name = "roomd_id")
+    private Long roomId;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "folder_id", nullable = false)
     private Folder folder;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "detail_id", nullable = false)
-    private MeetingDetail meetingDetail;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(name = "detail_id", nullable = false)
+//    private MeetingDetail meetingDetail;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
