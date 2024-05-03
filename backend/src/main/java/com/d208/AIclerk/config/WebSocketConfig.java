33bf21fc6d31f3965629727ab6aa132ab59fe2e1
김치욱 <car_ddong@naver.com>
2024-05-03 14:21:58 +0900

    Feat: CORS 에러 수정

--- backend/src/main/java/com/d208/AIclerk/config/WebSocketConfig.java
+++ backend/src/main/java/com/d208/AIclerk/config/WebSocketConfig.java
@@ -25,10 +25,10 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws").setAllowedOrigins("*");  //서버에 연결한 endpoint ws://localhost:8000/ws
-
+        registry.addEndpoint("/ws")
+                .setAllowedOriginPatterns("http://localhost:3000", "http://localhost:5173") // 구체적인 출처 지정
+                .withSockJS();
     }
-
 
 
 
