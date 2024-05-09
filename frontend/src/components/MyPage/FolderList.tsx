0b6b8f98ae2846d675c7a09dc388b96ba61787ed
박규리 <dw05131@naver.com>
2024-05-09 13:27:22 +0900

    fix: folder 타입 변경

--- frontend/src/components/MyPage/FolderList.tsx
+++ frontend/src/components/MyPage/FolderList.tsx
@@ -1,5 +1,5 @@
 import Folder from "./Folder";
-import { FolderType } from "@/types/MyPage";
+import { FolderData } from "@/types/MyPage";
 import { useQuery } from "@tanstack/react-query";
 import { fetchFolderList } from "@/services/MyPage";
 
@@ -18,7 +18,7 @@ const FolderList = () => {
   return (
     <div className="flex-[0.95] flex flex-col gap-[16px] h-full overflow-scroll overflow-x-hidden">
       {folders.length ? (
-        folders.map((folder: FolderType) => <Folder key={folder.folderId} folder={folder} />)
+        folders.map((folder: FolderData) => <Folder key={folder.folderId} folder={folder} />)
       ) : (
         <div className="w-full h-full flex justify-center items-center flex-col">
           <div>폴더가 없어요</div>
