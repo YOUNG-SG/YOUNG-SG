2fbaad30484c256ac7270c57cd8eeac74366c252
박규리 <dw05131@naver.com>
2024-05-16 15:09:29 +0900

    refactor: 타임라인 애니메이션 추가, 상태관리 기본값, 변수명 변경

--- frontend/src/components/MyPage/TimelineMonth.tsx
+++ frontend/src/components/MyPage/TimelineMonth.tsx
@@ -1,23 +1,50 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { clickButtonStore } from "@/store/myPageStore";
 import TimelineMeeting from "@/components/MyPage/TimelineMeeting";
-import UnfoldState from "@/assets/MyPage/CaretDown.svg?react"; // 펼친 상태
-import FoldState from "@/assets/MyPage/CaretRight.svg?react"; // 접힌 상태
+import ToggleButton from "@/assets/MyPage/CaretRight.svg?react"; // 접힌 상태
 import { TimelineMonthProps } from "@/types/MyPage";
 
 const TimelineMonth: React.FC<TimelineMonthProps> = ({ month, days }) => {
-  const { isClick, setIsClick, isAllExpanded } = clickButtonStore();
-  const [isShow, setIsShow] = useState(true);
+  const monthRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(true); // 각 month
+  const { allToggleActive, setAllToggleActive, clickAllOpen } = clickButtonStore(); // 모든 month
 
+  /* 처음에 maxHeight 설정 */
   useEffect(() => {
-    if (isClick) {
-      if (isAllExpanded) {
-        setIsShow(false);
-      } else {
-        setIsShow(true);
-      }
+    if (!monthRef || !monthRef.current) {
+      return;
     }
-  }, [isClick, isAllExpanded]);
+    // isOpen: true니까 maxHeight = scrollHeight
+    monthRef.current.style.maxHeight = `${isOpen ? monthRef.current.scrollHeight : 0}px`;
+  }, []);
+
+  /* AllToggleButton 눌렀을 때 handleAllToggle 실행 */
+  useEffect(() => {
+    if (allToggleActive) {
+      handleAllToggle();
+    }
+  }, [allToggleActive, clickAllOpen]);
+
+  /* 모든 month toggle 관리 */
+  const handleAllToggle = () => {
+    if (!monthRef || !monthRef.current) {
+      return;
+    }
+    // 모두펼치기 : 각 month 펼치기, maxHeight=scrollHeight
+    // 모두접기   : 각 month 접기,   maxHeight=0
+    monthRef.current.style.maxHeight = `${clickAllOpen ? monthRef.current.scrollHeight : 0}px`;
+    setIsOpen(clickAllOpen);
+  };
+
+  /* 각각의 month toggle 관리 */
+  const handleToggle = () => {
+    if (!monthRef || !monthRef.current) {
+      return;
+    }
+    // 펼친상태면 maxHeight = 0, 접힌상태면 maxHeight = scrollHeight
+    monthRef.current.style.maxHeight = `${isOpen ? 0 : monthRef.current.scrollHeight}px`;
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
@@ -25,24 +52,28 @@ const TimelineMonth: React.FC<TimelineMonthProps> = ({ month, days }) => {
         <div
           className="w-[34px] cursor-pointer"
           onClick={() => {
-            setIsShow(!isShow);
-            setIsClick(false);
+            handleToggle();
+            setAllToggleActive(false);
           }}
         >
-          {isShow ? <UnfoldState /> : <FoldState />}
+          {/* 각 month toggle 버튼 */}
+          <ToggleButton
+            className={`transition-rotate duration-300 ease-out ${isOpen ? "transform rotate-90" : ""}`}
+          />
         </div>
         <span className="w-[16px] h-[16px] mr-[20px] bg-[white] rounded-full"></span>
         <div className="text-[22px] font-b">{month}월</div>
       </div>
-      {/* 일 map 함수 */}
-      {isShow && (
-        <div className="flex flex-col gap-[16px] my-[16px]">
-          {days.map((day) => (
-            // meetingId: roomId
-            <TimelineMeeting key={day.meetingId} month={month} day={day} />
-          ))}
-        </div>
-      )}
+      {/* Day map 함수 */}
+      <div
+        className="flex flex-col gap-[16px] my-[16px] overflow-hidden transition-max-height duration-300 ease-out"
+        ref={monthRef}
+      >
+        {days.map((day) => (
+          // meetingId: roomId
+          <TimelineMeeting key={day.meetingId} month={month} day={day} />
+        ))}
+      </div>
     </div>
   );
 };
