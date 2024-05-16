7bf3886823c2301266a3a1b6d7f1a8f86902e9f3
박규리 <dw05131@naver.com>
2024-05-16 10:00:08 +0900

    refactor: login, navbar 로고 수정

--- frontend/src/components/@common/Navbar.tsx
+++ frontend/src/components/@common/Navbar.tsx
@@ -3,9 +3,13 @@ import { fetchMyProfile } from "@/services/MyPage";
 import { useQuery } from "@tanstack/react-query";
 import SkeletonLoader from "@/components/@common/SkeletonLoader";
 import DefaultProfile from "@/assets/@common/Profile.svg?react";
+import LogoImage from "@/assets/@common/LogoImage.svg?react";
+import LogoTitle from "@/assets/@common/LogoTitle.svg?react";
+import { useState } from "react";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  const [isHover, setIsHover] = useState(false);
 
   const {
     isLoading,
@@ -22,14 +26,18 @@ const Navbar = () => {
   }
 
   return (
-    <nav className="min-w-[80px] h-screen bg-[#777777] bg-opacity-40 backdrop-blur-4 flex flex-col justify-between items-center py-[40px]">
-      <img
-        src="../src/assets/@test/logo.png"
-        className="w-[30px] h-[30px] cursor-pointer"
-        onClick={() => {
-          navigate("/");
-        }}
-      />
+    <nav className="min-w-[80px] h-screen bg-[#777777] bg-opacity-40 backdrop-blur-4 flex flex-col justify-between items-center py-[35px]">
+      <div
+        onClick={() => navigate("/")}
+        onMouseOver={() => setIsHover(true)}
+        onMouseOut={() => setIsHover(false)}
+      >
+        {isHover ? (
+          <LogoImage className="w-[55px] h-[55px] cursor-pointer" />
+        ) : (
+          <LogoTitle className="w-[60px] h-[60px] cursor-pointer" />
+        )}
+      </div>
       {isLoading ? (
         <SkeletonLoader round="full" w={50} h={50} />
       ) : isError ? (
